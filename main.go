efc0e9a508fd13e341ac924c2dcd0f2ac9cc2653
Businge931 <jimmybusinge9@gmail.com>
2024-09-20 12:35:53 +0300

    feat: execute the CLI-app

---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+Copyright © 2024 BUSINGE BISANGA <busingebisanga99@gmail.com>
+*/
+package main
+
+import (
+	"github.com/Businge931/Kafka_and_CLIs/cmd"
+)
+
+func main() {
+	cmd.Execute()
+
+}
