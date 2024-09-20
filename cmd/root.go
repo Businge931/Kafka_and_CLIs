98cec892c2a8d6beb3761b4b9393a4d2b973017a
Businge931 <jimmybusinge9@gmail.com>
2024-09-20 17:57:27 +0300

    chore: clean up

--- cmd/root.go
+++ cmd/root.go
@@ -35,12 +35,4 @@ func Execute() {
 
 func SetupRootCmd() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.Kafka_and_CLIs.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 }
