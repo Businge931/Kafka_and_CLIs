a97f44b9d1f92fe98f42b74e5b3b92547e8119a3
Businge931 <jimmybusinge9@gmail.com>
2024-10-01 10:18:11 +0300

    refactor: fix formatting

--- service/messaging.go
+++ service/messaging.go
@@ -6,12 +6,8 @@ import (
 	"github.com/Businge931/Kafka_and_CLIs/models"
 )
 
-func (s *Service) SendMessage(
-	ctx context.Context,
-	 message models.Message) error {
-	return s.producer.ProduceMessage(
-		ctx,
-		 message)
+func (s *Service) SendMessage(ctx context.Context, message models.Message) error {
+	return s.producer.ProduceMessage(ctx, message)
 }
 
 func (s *Service) ReceiveMessages(ctx context.Context) ([]models.Message, error) {
