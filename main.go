0272c1739a4a1edbc693abf236850ff372b72ed7
Businge931 <jimmybusinge9@gmail.com>
2024-10-01 05:15:14 +0300

    feat: execute root and set-flag commands

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ Copyright © 2024 BUSINGE BISANGA <busingebisanga99@gmail.com>
 */
 package main
 
-import (
-	"github.com/Businge931/Kafka_and_CLIs/cmd"
-)
+import "github.com/Businge931/Kafka_and_CLIs/cmd"
 
 func main() {
+
 	// Call the setup functions to initialize commands
 	cmd.SetupRootCmd()
 	cmd.SetupReceiveCmd()
