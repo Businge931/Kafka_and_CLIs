ab374856a448087dcc7bcc62dae0f0354eb0f2bd
Businge931 <jimmybusinge9@gmail.com>
2024-10-07 08:57:28 +0300

    fix: add methods that correctly implement the service struct

--- service/messaging.go
+++ service/messaging.go
@@ -1,21 +1,9 @@
 package service
 
-// "github.com/Businge931/Kafka_and_CLIs/models"
-
 func (s *Service) SendMessage(topic, message string) error {
 	return s.producer.SendMessage(topic, message)
 }
 
-func (s *Service) ReceiveMessages(topic string, 
-	// testing bool
-	) error {
+func (s *Service) ReceiveMessages(topic string) error {
 	return s.consumer.ReadMessages(topic)
 }
-
-// func (s *Service) SendMessage(ctx context.Context, message models.Message) error {
-// 	return s.producer.ProduceMessage(ctx, message)
-// }
-
-// func (s *Service) ReceiveMessages(ctx context.Context) (message string,err error) {
-// 	return s.consumer.ConsumeMessages(ctx)
-// }
