843641d8fb5d965a5f9dd026a7d4cced6c3fbf42
Businge931 <jimmybusinge9@gmail.com>
2024-10-07 08:55:53 +0300

    refactor: remove un-used import and varibales

--- service/service.go
+++ service/service.go
@@ -1,9 +1,5 @@
 package service
 
-// "context"
-
-// "github.com/Businge931/Kafka_and_CLIs/models"
-
 type (
 	MessageProducer interface {
 		SendMessage(topic, message string) error
@@ -20,22 +16,6 @@ type (
 		consumer MessageConsumer
 	}
 )
-
-// type (
-// 	MessageProducer interface {
-// 		ProduceMessage(ctx context.Context, message models.Message) error
-// 	}
-// 	MessageConsumer interface {
-// 		ConsumeMessages(ctx context.Context) (
-// 			// []models.Message
-// 		message string,err error)
-// 	}
-
-	// Service struct {
-	// 	producer MessageProducer
-	// 	consumer MessageConsumer
-	// }
-// )
 
 func New(producer MessageProducer, consumer MessageConsumer) *Service {
 	return &Service{
