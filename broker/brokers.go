d9cddd3599eab0988c73a97ef575c1545f454d68
Businge931 <jimmybusinge9@gmail.com>
2024-10-11 23:47:09 +0300

    refactor: removed

--- broker/brokers.go
+++ broker/brokers.go
@@ -3,45 +3,45 @@ package broker
 // These are generic structs that implements the MessageProducer interface.
 // It can work with any messaging broker (e.g., Kafka, RabbitMQ).
 
-type Producer struct {
-	produceMessageFunc func(topic, message string) error
-	closeFunc          func()
-}
+// type Producer struct {
+// 	produceMessageFunc func(topic, message string) error
+// 	closeFunc          func()
+// }
 
-type Consumer struct {
-	consumeMessagesFunc func(topic string) error
-	closeFunc           func()
-}
+// type Consumer struct {
+// 	consumeMessagesFunc func(topic string) error
+// 	closeFunc           func()
+// }
 
-func NewProducer(produceFunc func(topic, message string) error, closeFunc func()) *Producer {
-	return &Producer{
-		produceMessageFunc: produceFunc,
-		closeFunc:          closeFunc,
-	}
-}
+// func NewProducer(produceFunc func(topic, message string) error, closeFunc func()) *Producer {
+// 	return &Producer{
+// 		produceMessageFunc: produceFunc,
+// 		closeFunc:          closeFunc,
+// 	}
+// }
 
-func (p *Producer) SendMessage(topic, message string) error {
-	return p.produceMessageFunc(topic, message)
-}
+// func (p *Producer) SendMessage(topic, message string) error {
+// 	return p.produceMessageFunc(topic, message)
+// }
 
-func (p *Producer) Close() {
-	p.closeFunc()
-}
+// func (p *Producer) Close() {
+// 	p.closeFunc()
+// }
 
-func NewConsumer(consumeFunc func(topic string) error, closeFunc func()) *Consumer {
-	return &Consumer{
-		consumeMessagesFunc: consumeFunc,
-		closeFunc:           closeFunc,
-	}
-}
+// func NewConsumer(consumeFunc func(topic string) error, closeFunc func()) *Consumer {
+// 	return &Consumer{
+// 		consumeMessagesFunc: consumeFunc,
+// 		closeFunc:           closeFunc,
+// 	}
+// }
 
-func (c *Consumer) ReadMessages(topic string) error {
-	return c.consumeMessagesFunc(topic)
-}
+// func (c *Consumer) ReadMessages(topic string) error {
+// 	return c.consumeMessagesFunc(topic)
+// }
 
-func (c *Consumer) Close() {
-	c.closeFunc()
-}
+// func (c *Consumer) Close() {
+// 	c.closeFunc()
+// }
 
 //////*************************************************************
 
