b290c368f46535838c8f5c5ce7a6c2caa8165c63
Businge931 <jimmybusinge9@gmail.com>
2024-10-23 12:01:22 +0300

    refactor(tests): add test cases for service

--- service/messaging_test.go
+++ service/messaging_test.go
@@ -1,147 +1,149 @@
 package service_test
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// 	"go.uber.org/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
 
-// 	"github.com/Businge931/Kafka_and_CLIs/models"
-// 	"github.com/Businge931/Kafka_and_CLIs/service"
-// 	mock_service "github.com/Businge931/Kafka_and_CLIs/service/mock"
-// )
+	"github.com/Businge931/Kafka_and_CLIs/models"
+	"github.com/Businge931/Kafka_and_CLIs/service"
+	mock_service "github.com/Businge931/Kafka_and_CLIs/service/mock"
+)
 
-// func TestService_SendMessage(t *testing.T) {
-// 	type args struct {
-// 		topic   string
-// 		message string
-// 	}
+func TestSendMessage(t *testing.T) {
+	type args struct {
+		topic   string
+		message string
+	}
+	tests := []struct {
+		name           string
+		args           args
+		mockProducerFn func(mock *mock_service.MockMessageProducer)
+		wantErr        error
+	}{
+		{
+			name: "success/message send",
+			args: args{
+				topic:   "myTopic",
+				message: "Hello, Kafka!",
+			},
+			mockProducerFn: func(mock *mock_service.MockMessageProducer) {
+				mock.EXPECT().SendMessage("myTopic", "Hello, Kafka!").Return(nil).Times(1)
+			},
+			wantErr: nil,
+		},
+		{
+			name: "error/Empty topic ",
+			args: args{
+				topic:   "",
+				message: "Hello, Kafka!",
+			},
+			mockProducerFn: nil,
+			wantErr:        models.ErrEmptyTopic,
+		},
+		{
+			name: "error/Empty message ",
+			args: args{
+				topic:   "myTopic",
+				message: "",
+			},
+			mockProducerFn: nil,
+			wantErr:        models.ErrEmptyMessage,
+		},
+		{
+			name: "error/Retry on failure",
+			args: args{
+				topic:   "myTopic",
+				message: "Hello, Kafka!",
+			},
+			mockProducerFn: func(mock *mock_service.MockMessageProducer) {
+				mock.EXPECT().SendMessage("myTopic", "Hello, Kafka!").Return(models.ErrSendFail).Times(5)
+			},
+			wantErr: models.ErrMaxRetry,
+		},
+	}
 
-// 	tests := []struct {
-// 		name        string
-// 		args        args
-// 		before      func(producer *mock_service.MockMessageProducer)
-// 		expectedErr error
-// 	}{
-// 		{
-// 			name: "success/message send",
-// 			args: args{
-// 				topic:   "myTopic",
-// 				message: "hello world",
-// 			},
-// 			before: func(producer *mock_service.MockMessageProducer) {
-// 				producer.EXPECT().SendMessage("myTopic", "hello world").Return(nil)
-// 			},
-// 			expectedErr: nil,
-// 		},
-// 		{
-// 			name: "error/empty topic",
-// 			args: args{
-// 				topic:   "",
-// 				message: "hello world",
-// 			},
-// 			before:      func(_ *mock_service.MockMessageProducer) {},
-// 			expectedErr: models.ErrEmptyTopic,
-// 		},
-// 		{
-// 			name: "error/empty message",
-// 			args: args{
-// 				topic:   "myTopic",
-// 				message: "",
-// 			},
-// 			before:      func(_ *mock_service.MockMessageProducer) {},
-// 			expectedErr: models.ErrEmptyMessage,
-// 		},
-// 		{
-// 			name: "error/producer error",
-// 			args: args{
-// 				topic:   "myTopic",
-// 				message: "hello world",
-// 			},
-// 			before: func(producer *mock_service.MockMessageProducer) {
-// 				producer.EXPECT().SendMessage("myTopic", "hello world").Return(models.ErrSendFail)
-// 			},
-// 			expectedErr: models.ErrSendMessageToTopic,
-// 		},
-// 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			ctrl := gomock.NewController(t)
-// 			defer ctrl.Finish()
+			mockProducer := mock_service.NewMockMessageProducer(ctrl)
+			if tt.mockProducerFn != nil {
+				tt.mockProducerFn(mockProducer)
+			}
 
-// 			mockProducer := mock_service.NewMockMessageProducer(ctrl)
-// 			tt.before(mockProducer)
+			svc := service.New(mockProducer, nil)
+			err := svc.SendMessage(tt.args.topic, tt.args.message)
 
-// 			svc := service.New(mockProducer, nil)
-// 			err := svc.SendMessage(tt.args.topic, tt.args.message)
+			if tt.wantErr != nil {
+				assert.ErrorIs(t, err, tt.wantErr)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
 
-// 			if tt.expectedErr != nil {
-// 				assert.EqualError(t, err, tt.expectedErr.Error())
-// 			} else {
-// 				assert.NoError(t, err)
-// 			}
-// 		})
-// 	}
-// }
+func TestReadMessages(t *testing.T) {
+	type args struct {
+		topic string
+	}
+	tests := []struct {
+		name           string
+		args           args
+		mockConsumerFn func(mock *mock_service.MockMessageConsumer)
+		wantErr        error
+	}{
+		{
+			name: "Successful message read",
+			args: args{
+				topic: "myTopic",
+			},
+			mockConsumerFn: func(mock *mock_service.MockMessageConsumer) {
+				mock.EXPECT().ReadMessages("myTopic").Return(nil).Times(1)
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Empty topic error",
+			args: args{
+				topic: "",
+			},
+			mockConsumerFn: nil,
+			wantErr:        models.ErrEmptyTopic,
+		},
+		{
+			name: "Failed to read messages",
+			args: args{
+				topic: "myTopic",
+			},
+			mockConsumerFn: func(mock *mock_service.MockMessageConsumer) {
+				mock.EXPECT().ReadMessages("myTopic").Return(models.ErrReadFail).Times(1)
+			},
+			wantErr: models.ErrReadFail,
+		},
+	}
 
-// func TestService_ReceiveMessages(t *testing.T) {
-// 	type args struct {
-// 		topic string
-// 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-// 	tests := []struct {
-// 		name        string
-// 		args        args
-// 		before      func(consumer *mock_service.MockMessageConsumer)
-// 		expectedErr error
-// 	}{
-// 		{
-// 			name: "success/message receive",
-// 			args: args{
-// 				topic: "myTopic",
-// 			},
-// 			before: func(consumer *mock_service.MockMessageConsumer) {
-// 				consumer.EXPECT().ReadMessages("myTopic").Return(nil)
-// 			},
-// 			expectedErr: nil,
-// 		},
-// 		{
-// 			name: "error/empty topic",
-// 			args: args{
-// 				topic: "",
-// 			},
-// 			before:      func(_ *mock_service.MockMessageConsumer) {},
-// 			expectedErr: models.ErrEmptyTopic,
-// 		},
-// 		{
-// 			name: "error/consumer error",
-// 			args: args{
-// 				topic: "myTopic",
-// 			},
-// 			before: func(consumer *mock_service.MockMessageConsumer) {
-// 				consumer.EXPECT().ReadMessages("myTopic").Return(models.ErrReadFail)
-// 			},
-// 			expectedErr: models.ErrReceiveMessageFromTopic,
-// 		},
-// 	}
+			mockConsumer := mock_service.NewMockMessageConsumer(ctrl)
+			if tt.mockConsumerFn != nil {
+				tt.mockConsumerFn(mockConsumer)
+			}
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			ctrl := gomock.NewController(t)
-// 			defer ctrl.Finish()
+			svc := service.New(nil, mockConsumer)
+			err := svc.ReadMessages(tt.args.topic)
 
-// 			mockConsumer := mock_service.NewMockMessageConsumer(ctrl)
-// 			tt.before(mockConsumer)
-
-// 			svc := service.New(nil, mockConsumer)
-// 			err := svc.ReceiveMessages(tt.args.topic)
-
-// 			if tt.expectedErr != nil {
-// 				assert.EqualError(t, err, tt.expectedErr.Error())
-// 			} else {
-// 				assert.NoError(t, err)
-// 			}
-// 		})
-// 	}
-// }
+			if tt.wantErr != nil {
+				assert.ErrorContains(t, err, tt.wantErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
